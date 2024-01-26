445de3118d8fa9b86a06e912c0369d49c2f5d8a1
Olukayode <olukayodeazeez2000@gmail.com>
2024-01-26 16:10:32 +0100

    Responsive

--- src/utils/Firebase.js
+++ src/utils/Firebase.js
@@ -1,17 +1,23 @@
 import { initializeApp } from 'firebase/app'
 import { getAuth } from 'firebase/auth'
 
+// const firebaseConfig = {
+//   apiKey: 'AIzaSyCees2-MkZojL_-gESpS-jVjQZWqVV7g7s',
+//   authDomain: 'trasfy-authentification.firebaseapp.com',
+//   projectId: 'trasfy-authentification',
+//   storageBucket: 'trasfy-authentification.appspot.com',
+//   messagingSenderId: '321858426382',
+//   appId: '1:321858426382:web:5a61f2081d8981dc400e5f',
+// }
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyCees2-MkZojL_-gESpS-jVjQZWqVV7g7s',
-  authDomain: 'trasfy-authentification.firebaseapp.com',
-  projectId: 'trasfy-authentification',
-  storageBucket: 'trasfy-authentification.appspot.com',
-  messagingSenderId: '321858426382',
-  appId: '1:321858426382:web:5a61f2081d8981dc400e5f',
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 }
 
 const app = initializeApp(firebaseConfig)
 export const firebaseAuth = getAuth(app)
-
-
