64595dc44edc3dd87c39cdc47a9b2a37c92c54b9
Olukayode <olukayodeazeez2000@gmail.com>
2024-01-26 16:27:53 +0100

    Transfy development

--- src/utils/Firebase.js
+++ src/utils/Firebase.js
@@ -1,15 +1,6 @@
 import { initializeApp } from 'firebase/app'
 import { getAuth } from 'firebase/auth'
 
-// const firebaseConfig = {
-//   apiKey: 'AIzaSyCees2-MkZojL_-gESpS-jVjQZWqVV7g7s',
-//   authDomain: 'trasfy-authentification.firebaseapp.com',
-//   projectId: 'trasfy-authentification',
-//   storageBucket: 'trasfy-authentification.appspot.com',
-//   messagingSenderId: '321858426382',
-//   appId: '1:321858426382:web:5a61f2081d8981dc400e5f',
-// }
-
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
